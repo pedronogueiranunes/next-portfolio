e47bc5d1775727a8de81b0cd7e6d8bdd786d78ff
Pedro Nogueira <pedronogueiranunes@gmail.com>
2022-05-17 15:12:31 -0300

    project configs

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const option = {
+    arrowParens: "avoid",
+    singleQuote: true,
+    brackerSpacing: true,
+    endOfLine: "lf",
+    semi: false,
+    tabWidth: 2,
+    trailingComma: "none"
+}
+
+module.exports = option;
